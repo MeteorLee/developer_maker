53da838a04b80c82d055d1d91b5504667f241aae
HoSeung <meteorproject529@gamil.com>
2023-02-28 17:06:57 +0900

    Feat: Test 구현
    ControllerTest 구현

---
+++ src/main/java/com/fastcam/programming/dmaker/config/JpaConfig.java
@@ -0,0 +1,17 @@
+package com.fastcam.programming.dmaker.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * packageName :  com.fastcam.programming.dmaker.config
+ * fileName : JpaAuditingConfiguration
+ * author :  82108
+ * date : 2023-02-28
+ * description :
+ */
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+}
